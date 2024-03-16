66e4af8d32f34c1caa7523a5d05c666c5ea9475d
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-03-16 14:11:33 +0700

    Time: 1 ms (98.66%), Space: 41.3 MB (64.40%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,31 +3,25 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray())
         {
-            if (c == '(' || c =='[' || c =='{')
+            if (c == '(' || c == '[' || c == '{')
             {
-                stack.add(c);
+                stack.push(c);
             }
             else
             {
                 if (stack.isEmpty())
-                {
                     return false;
-                }
+                
                 char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                        (c == ']' && top != '[') ||
-                        (c == '}' && top != '{'))
+                if ((c == ')' && top != '(')
+                   || (c == ']' && top != '[')
+                   || (c == '}' && top != '{'))
                 {
                     return false;
                 }
             }
         }
         
-        // Chỗ này nghĩa là nếu stack còn ký tự nào là sai luôn
-        // Bởi vì nếu còn thì chuỗi ban đầu vẫn còn đang trạng thái mở
         return stack.isEmpty();
-        // if (stack.isEmpty())
-        //     return true;
-        // return false;
     }
 }
