18044b96d4a92258e09b64ad5e9de5287fac64f3
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-03-16 13:54:22 +0700

    Time: 50 ms (11.36%), Space: 44.6 MB (85.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-        int i_index, j_index;
-        for (int i = 0 ; i < nums.length; i++)
+        int[] results = new int [2];
+        for (int i = 0; i < nums.length; i++)
         {
             for (int j = i + 1; j < nums.length; j++)
             {
                 if (target == nums[i] + nums[j])
                 {
-                    i_index = i;
-                    j_index = j;
-                    arr[0] = i_index;
-                    arr[1] = j_index;
+                    results[0] = i;
+                    results[1] = j;
                 }
             }
         }
-        return arr;
+        return results;
     }
 }
