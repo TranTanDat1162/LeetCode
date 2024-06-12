7b1661121697eaff174056c5a3ca34275798264c
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-06-12 16:41:17 +0700

    Time: 2 ms (97.74%), Space: 45 MB (13.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,8 +3,9 @@ class Solution {
         HashMap<Integer, Integer> hashMap = new HashMap();
         
         for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(target - nums[i])) {
-                return new int[] {hashMap.get(target - nums[i]), i};
+            int complement = target - nums[i];
+            if (hashMap.containsKey(complement)) {
+                return new int[] {i, hashMap.get(complement)};
             }
             hashMap.put(nums[i], i);
         }
