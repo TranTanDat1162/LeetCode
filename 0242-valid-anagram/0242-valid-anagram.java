d1d7cb8426cb616024a249deb682cf653bf91b25
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-06-12 16:28:37 +0700

    Time: 26 ms (7.16%), Space: 42.6 MB (95.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        if (s.length() != t.length())
             return false;
-        }
-
-        HashMap<Character, Integer> map = new HashMap<>();
+        
+        HashMap<Character, Integer> hashMap = new HashMap();
+        
         for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+            hashMap.put(s.charAt(i), hashMap.getOrDefault(s.charAt(i), 0) + 1);
+            hashMap.put(t.charAt(i), hashMap.getOrDefault(t.charAt(i), 0) - 1);
         }
-        return map.values().stream().allMatch(i -> i == 0 ? true : false);
+        
+        return hashMap.values().stream().allMatch(i -> i == 0);
     }
 }
