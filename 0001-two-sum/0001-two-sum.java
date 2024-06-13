fb72b39617e7bc6b7424659c6badf48922c8993a
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-06-13 13:16:07 +0700

    Time: 2 ms (97.78%), Space: 44.8 MB (51.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hashMap = new HashMap();
-        
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (hashMap.containsKey(complement)) {
@@ -9,7 +8,6 @@ class Solution {
             }
             hashMap.put(nums[i], i);
         }
-        
         return new int[] {-1, -1};
     }
 }
