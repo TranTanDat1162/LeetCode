c5524153786943a5fbc292b2582d61e2c499863e
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-06-14 10:25:43 +0700

    Time: 14 ms (25.89%), Space: 49.5 MB (74.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,9 @@
 class Solution {
-    public static int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+    public int majorityElement(int[] nums) {
+                HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             hashMap.put(nums[i], hashMap.getOrDefault(nums[i], 0) + 1);
         }
-        return hashMap.keySet().stream().max(Comparator.comparing(hashMap::get)).get();
+        return hashMap.keySet().stream().max(Comparator.comparing(hashMap::get)).get().intValue();
     }
 }
