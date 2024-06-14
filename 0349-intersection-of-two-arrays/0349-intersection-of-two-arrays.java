2a9339208d74a36c8371fcef6b55c1fcc3d5c7f4
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-06-14 14:38:07 +0700

    Time: 16 ms (6.44%), Space: 43 MB (67.67%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,6 +1,6 @@
 class Solution {
-    public static int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> list = Arrays.stream(nums1).boxed().collect(Collectors.toList());
-        return Arrays.stream(nums2).distinct().filter(list::contains).toArray();
+    public int[] intersection(int[] nums1, int[] nums2) {
+                List<Integer> list = Arrays.stream(nums1).boxed().collect(Collectors.toList());
+        return Arrays.stream(nums2).boxed().filter(list::contains).distinct().mapToInt(i -> i).toArray();
     }
 }
