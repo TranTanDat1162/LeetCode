d5cac440c3879bc46082543eb38031d4510f69d5
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-07-15 17:34:18 +0700

    Time: 0 ms (100.00%), Space: 41.4 MB (55.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,15 +1,20 @@
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        String prefix = strs[0];
-        
-        for (String str : strs)
-        {
-            while (str.indexOf(prefix) != 0)
-            {
-                prefix = prefix.substring(0, prefix.length() - 1);
-            }
+import java.util.*;
+
+public class Solution {
+
+    public static String longestCommonPrefix(String[] strings) {
+        String result = strings[0];
+
+        for (String str: strings) {
+            while (str.indexOf(result) != 0)
+                result = result.substring(0, result.length() - 1);
         }
-        
-        return prefix;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] strings = new String[] {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strings));
     }
 }
