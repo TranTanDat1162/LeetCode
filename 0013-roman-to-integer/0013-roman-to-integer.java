37f586bcdd3912e59a5a82006231e946971a2cf8
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-07-15 15:16:13 +0700

    Time: 6 ms (29.81%), Space: 45.1 MB (9.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,33 +1,37 @@
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> charToInt = new HashMap<>();
-        
-        charToInt.put('I', 1);
-        charToInt.put('V', 5);
-        charToInt.put('X', 10);
-        charToInt.put('L', 50);
-        charToInt.put('C', 100);
-        charToInt.put('D', 500);
-        charToInt.put('M', 1000);
-        
+import java.util.*;
+
+public class Solution {
+
+    public static int romanToInt(String s) {
+        // Create hash map for storing each character into value in roman number
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        // Mapping data
+        hashMap.put('I', 1);
+        hashMap.put('V', 5);
+        hashMap.put('X', 10);
+        hashMap.put('L', 50);
+        hashMap.put('C', 100);
+        hashMap.put('D', 500);
+        hashMap.put('M', 1000);
+
+        // Create the result variable to store the integer from String s
         int result = 0;
-        int preValue = 0;
-        
-        for (char c : s.toCharArray())
-        {
-            int currentValue = charToInt.get(c);
-            if (preValue < currentValue)
-            {
-                result += currentValue - 2 * preValue;
-            }
+
+        // Looping
+        for (int i = 0; i < s.length(); i++) {
+            // If there is the next character is greater than before it, make the logic the result
+            if (i > 0 && hashMap.get(s.charAt(i)) > hashMap.get(s.charAt(i - 1)))
+                result += hashMap.get(s.charAt(i)) - 2 * hashMap.get(s.charAt(i - 1));
             else
-            {
-                result += currentValue;
-            }
-            
-            preValue = currentValue;
+                result += hashMap.get(s.charAt(i));
         }
-        
+
         return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "III";
+        System.out.println(romanToInt(s));
     }
 }
