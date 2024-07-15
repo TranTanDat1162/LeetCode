5a079f185b89b1b3236d69fd2a79c8a6ef0e0609
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-07-15 13:55:06 +0700

    Time: 8 ms (11.65%), Space: 44 MB (27.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        // Create StringBuilder instance
         StringBuilder sb = new StringBuilder(String.valueOf(x));
 
-        // reverse the string
+        // Call function reverse from StringBuilder to reverse String x
         sb.reverse();
 
-        // check if the reversed string is equal to the original string
+        // Return for checking whether reversed string is the same as string x
         return String.valueOf(x).contentEquals(sb);
+        
+        /**
+            Please note that using contentEquals for StringBuilder or StringBuffer        
+         */
     }
 }
