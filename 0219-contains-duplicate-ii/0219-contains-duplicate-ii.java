49f793875121df3853c363e35c1da4daa4802ef0
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-07-17 15:23:07 +0700

    Time: 23 ms (20.02%), Space: 57.9 MB (20.36%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,11 +1,9 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-                HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(nums[i]) && i - hashMap.get(nums[i]) <= k) {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0 ; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i]) && (i - hashMap.get(nums[i]) <= k))
                 return true;
-            }
             hashMap.put(nums[i], i);
         }
         return false;
