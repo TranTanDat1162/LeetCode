55ede9fef6ccd9976589bb74f944a476537efccb
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-07-17 12:54:52 +0700

    Time: 1 ms (98.97%), Space: 41.3 MB (73.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,11 +1,13 @@
-import java.util.*;
-
-public class Solution {
-
-    public static boolean isValid(String s) {
+class Solution {
+    public boolean isValid(String s) {
+        // Create the stack containing close bracket from string s
         Stack<Character> closeBracketStack = new Stack<>();
 
+        // Check every single character in string s
         for (char c : s.toCharArray()) {
+            /**
+            * Catch if c is the open bracket, the stack will push the approriate close bracket
+             */
             if (c == '(')
                 closeBracketStack.push(')');
 
@@ -14,17 +16,18 @@ public class Solution {
 
             else if (c == '{')
                 closeBracketStack.push('}');
-            
-            else if (closeBracketStack.isEmpty() || closeBracketStack.pop() != c) 
+
+            // Firstly, we check if there is no open bracket in string before
+            // It can be infered that theire is no close in close bracket => false
+            // Then, check in close bracket, if the next characer is not the same as pop up from
+            // stack => false 
+            else if (closeBracketStack.isEmpty() || closeBracketStack.pop() != c)
                 return false;
         }
-        
+
+        // Supposing the string s = "()["
+        // For looping above, definitely we have "[" left, so we check if
+        // stack is empty or not
         return closeBracketStack.isEmpty();
-    }
-
-    public static void main(String[] args) {
-        String s = "()[]";
-        System.out.println(isValid(s));
-
     }
 }
