e1254bc8c55b0cc4c89987c8bc8a9931dbac26df
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-07-17 14:25:42 +0700

    Time: 32 ms (5.13%), Space: 57.4 MB (70.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-                int[] listDistinct = Arrays.stream(nums).distinct().toArray();
+        // Create the list distinct
+        int[] listDistinct = Arrays.stream(nums).distinct().toArray();
+
+        // Check if list num is not the same as listDistinct
         return !Arrays.equals(nums, listDistinct);
     }
 }
