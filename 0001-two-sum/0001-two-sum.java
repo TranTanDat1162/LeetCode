e9664a8cc26a11d23688350e6cc8d4a785f7fe5f
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-06 20:07:41 +0700

    Time: 2 ms (98.05%), Space: 45 MB (26.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Create a hash map for storing value as loop number
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        
+       HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            // Check if hashMap contains the key for target - nums[i]
-            if (hashMap.containsKey(target - nums[i])) {
-                return new int[] {hashMap.get(target - nums[i]), i};
+            int complement = target - nums[i];
+            if (hashMap.containsKey(complement)) {
+                return new int[] {i, hashMap.get(complement)};
             }
-
-            // Mapping elements in nums array into varible i loop
             hashMap.put(nums[i], i);
         }
-
-        // If there is no any sum of two numbers for target, return {-1, -1} or throw something error
         return new int[] {-1, -1};
     }
 }
