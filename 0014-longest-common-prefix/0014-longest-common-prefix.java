769d3d4a08ca552b207fcd75b3c5a4058583ace5
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-07 08:40:28 +0700

    Time: 0 ms (100.00%), Space: 41.3 MB (73.74%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // Create variable prefix for strs[0] assigned to
-        String prefix = strs[0];
-
-        // Loop
-        for (String str : strs) {
-            // Check if the prefix is appeared in other strs array
-            // Update for prefix
-            while (str.indexOf(prefix) != 0)
-                prefix = prefix.substring(0, prefix.length() - 1);
+        String temp = strs[0];
+        
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(temp) != 0) 
+                temp = temp.substring(0, temp.length() - 1);
         }
-
-        return prefix;
+        
+        return temp;
     }
 }
