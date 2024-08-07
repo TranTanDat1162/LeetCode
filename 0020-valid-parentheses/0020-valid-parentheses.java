191feb31c26e29dd462bcd6cd0e7deec2fe8ab35
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-07 08:49:17 +0700

    Time: 1 ms (98.90%), Space: 41.3 MB (78.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,33 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        // Create the stack containing close bracket from string s
-        Stack<Character> closeBracketStack = new Stack<>();
-
-        // Check every single character in string s
+        Stack<Character> closedBrackets = new Stack<>();
+        
         for (char c : s.toCharArray()) {
-            /**
-            * Catch if c is the open bracket, the stack will push the approriate close bracket
-             */
-            if (c == '(')
-                closeBracketStack.push(')');
-
-            else if (c == '[')
-                closeBracketStack.push(']');
-
-            else if (c == '{')
-                closeBracketStack.push('}');
-
-            // Firstly, we check if there is no open bracket in string before
-            // It can be infered that theire is no close in close bracket => false
-            // Then, check in close bracket, if the next characer is not the same as pop up from
-            // stack => false 
-            else if (closeBracketStack.isEmpty() || closeBracketStack.pop() != c)
+            if (c == '(') {
+                closedBrackets.push(')');
+            }
+                
+            else if (c == '[') {
+                closedBrackets.push(']');
+            }
+            
+            else if (c == '{') {
+                closedBrackets.push('}');
+            }
+                
+            else if (closedBrackets.isEmpty() || c != closedBrackets.pop())
                 return false;
         }
-
-        // Supposing the string s = "()["
-        // For looping above, definitely we have "[" left, so we check if
-        // stack is empty or not
-        return closeBracketStack.isEmpty();
+                
+        return closedBrackets.isEmpty();
     }
 }
