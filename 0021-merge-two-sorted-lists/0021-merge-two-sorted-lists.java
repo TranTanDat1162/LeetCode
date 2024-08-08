e14632b23e767bce630fb0eda99229be02b790fa
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-08 09:43:05 +0700

    Time: 0 ms (100.00%), Space: 42.1 MB (84.09%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,15 +10,10 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Create new list node
         ListNode head = new ListNode();
-        
-        // Set the pointer to the tail;
         ListNode tail = head;
         
-        // Iteration for list1 and list2 to add value for ListNode result
         while (list1 != null && list2 != null) {
-            // Check if list note of list1 or list2 is smaller
             if (list1.val <= list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
@@ -30,10 +25,7 @@ class Solution {
             tail = tail.next;
         }
         
-        // Supposing if list1 or list2 contains other list node => set the pointer of tail to this list
         tail.next = list1 != null ? list1 : list2;
-        
-        // Return the list node
         return head.next;
     }
 }
