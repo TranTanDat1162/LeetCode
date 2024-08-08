a12d8610c50b2fadb8e85305d113f9f061175e39
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-08 17:15:23 +0700

    Time: 14 ms (25.11%), Space: 49.2 MB (84.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,11 +1,11 @@
 class Solution {
-    public static int majorityElement(int[] nums) {
-        // Create the hash map for storing num of appear of elements (key)
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int num: nums)
-            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
-
-        // Return the key having the max value
-        return hashMap.keySet().stream().max(Comparator.comparing(hashMap::get)).orElse(0);
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        return map.entrySet().stream().max(Map.Entry.comparingByValue())
+            .map(Map.Entry::getKey).orElseThrow();
     }
 }
