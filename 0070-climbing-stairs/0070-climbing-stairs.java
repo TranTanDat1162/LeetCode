eaf9a0a429c7ac0ddf55797e98f986f76473fd1f
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-08 15:35:25 +0700

    Time: 0 ms (100.00%), Space: 40.2 MB (60.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,7 +8,9 @@
 */
 class Solution {
     public int climbStairs(int n) {
-        int a = 1, b = 1, c;
+        int a = 1;
+        int b = 1;
+        int c;
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
