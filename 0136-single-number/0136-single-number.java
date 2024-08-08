f5c81a7ae0e82501479bfe211b1f2fdb4bb747e6
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-08 16:31:48 +0700

    Time: 18 ms (10.21%), Space: 44.9 MB (92.03%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -4,8 +4,14 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        return map.entrySet().stream().filter(e -> e.getValue() == 1)
-            .map(Map.Entry::getKey)
-                      .findFirst().orElseThrow();
+        
+        // Cach 1
+        // return map.entrySet().stream().filter(e -> e.getValue() == 1)
+        //     .map(Map.Entry::getKey)
+        //               .findFirst().orElseThrow();
+        
+        
+        // Cach 2
+        return map.entrySet().stream().min(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElseThrow();
     }
 }
