cb8f2f00fef11d0f387fa682828544d7b4caaea4
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-08 15:26:54 +0700

    Time: 0 ms (100.00%), Space: 40.3 MB (45.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,14 @@
+/*
+    n = 1 => 1
+    n = 2 => 2
+    n = 3 => 3
+    n = 4 => 5
+    n = 5 => 8
+    => 1 2 3 5 8 13 21 34 (fibonanci)
+*/
 class Solution {
     public int climbStairs(int n) {
         int a = 1, b = 1, c;
-        if (n == 0)
-            return a;
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
