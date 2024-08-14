53a366f2572ebadd3fe18d2fcf6865ed9d5a5231
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-14 08:39:42 +0700

    Time: 12 ms (5.25%), Space: 45.5 MB (18.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,18 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        // Sort the array
         Arrays.sort(nums);
-
+        
+        if (nums[nums.length - 1] == nums.length - 1)
+            return nums.length;
+        
+        // Create hashmap
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            hashMap.put(nums[i], i);
-            if (hashMap.containsKey(hashMap.get(nums[i]) + 1)) {
-                return i;
-            }
+            map.put(nums[i], i);
         }
-        return nums.length;
+        
+        return map.entrySet().stream().filter(e -> e.getKey() == e.getValue() + 1)
+                .map(Map.Entry::getValue).findFirst().orElseThrow();
     }
 }
