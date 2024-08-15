ffd302ba02084e12ca0f58c262f8c1138987939d
Trần Tấn Đạt <126229682+TranTanDat1162@users.noreply.github.com>
2024-08-15 12:24:27 +0700

    Time: 34 ms (5.30%), Space: 58.6 MB (33.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) 
-                return true;
-            map.put(nums[i], i);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return false;
+        return map.entrySet().stream().anyMatch(i -> i.getValue() >= 2);
     }
 }
